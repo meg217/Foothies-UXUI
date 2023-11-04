1dd52566a0163b465b6f68068d0abf0771b4a991
JDMxSpartan <kevindehoyos66066@gmail.com>
2023-11-03 23:49:06 -0500

    messed with Modal

--- public/js/loginModal.js
+++ public/js/loginModal.js
@@ -1,25 +1,19 @@
 // Get the modal
-var modal = document.getElementById("loginModal");
+var login_modal = document.getElementById("loginModal");
+var closeButton = document.querySelector("#loginModal .close");
+var loginBtn = document.getElementById("loginButton");
 
-// Get the button that opens the modal
-var btn = document.getElementById("loginButton");
+loginBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  login_modal.style.display = "block";
+});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+closeButton.addEventListener("click", function () {
+  login_modal.style.display = "none";
+});
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener("click", function (event) {
+  if (event.target === login_modal) {
+    login_modal.style.display = "none";
   }
-}
+});
