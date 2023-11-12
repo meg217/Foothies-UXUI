66a134e0d6e6f1f05408a8b51efd3eec32924174
meg217 <117707592+meg217@users.noreply.github.com>
2023-11-12 01:37:37 -0600

    add to cart increase number when clicked

--- public/js/add_to_cart.js
+++ public/js/add_to_cart.js
@@ -5,15 +5,19 @@ document
   .forEach(function (addToCartButton) {
     addToCartButton.addEventListener("click", function () {
       addToCartButton.classList.add("added");
+      
+            // Simulate updating the cart total
+            var cart = document.getElementById('cart');
+            var cartTotal = parseInt(cart.getAttribute('data-totalitems')) || 0;
+            var newCartTotal = cartTotal + 1;
+      
+            cart.setAttribute('data-totalitems', newCartTotal);
+      
+            var cartIcon = cart.querySelector('.bi-cart');
+            cartIcon.textContent = newCartTotal;
     setTimeout(function () {
       addToCartButton.classList.remove("added");
 
-      // Simulate updating the cart total
-      var cart = document.getElementById('cart');
-      var cartTotal = parseInt(cart.getAttribute('data-totalitems')) || 0;
-      var newCartTotal = cartTotal + 1;
-
-      cart.setAttribute('data-totalitems', newCartTotal);
     }, 2000);
     });
   });
