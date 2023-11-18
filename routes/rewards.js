0e1b5c2a5e1e66b95bf983d7918b64f49242e66f
JDMxSpartan <kevindehoyos66066@gmail.com>
2023-11-17 22:33:50 -0600

    points now add up and show onto rewards page

--- routes/rewards.js
+++ routes/rewards.js
@@ -1,12 +1,32 @@
-//rewards route
-
-const express = require('express');
+// routes/rewards.js
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const User = require("../models/user");
 
-// Display the order page
-router.get('/', (req, res) => {
-    res.render('rewards');
-  });
+// Display the rewards page
+router.get("/", (req, res) => {
+  // Check if the user is logged in
+  if (!req.session.user) {
+    return res.redirect("/login.html");
+  }
 
-  module.exports = router;
+  const userId = req.session.user._id;
+
+  // Fetch the user and their points
+  User.findById(userId)
+    .then((user) => {
+      if (user) {
+        // Render the rewards page with the user's points
+        res.render("rewards", { points: user.points });
+      } else {
+        // Handle the case where the user is not found
+        res.status(404).send("User not found");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+module.exports = router;
